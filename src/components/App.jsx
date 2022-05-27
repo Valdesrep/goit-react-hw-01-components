31d027b12abf3fc5ebc8a7ca433ab742a3365bd2
Valdesrep <90558354+Valdesrep@users.noreply.github.com>
2022-05-27 13:30:40 +0300

    fisnish Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import { Profile } from './Profile/Profile';
-// import { Statistics } from './Statistics/Statistics';
+import { Statistics } from './Statistics/Statistics';
 import user from './Profile/user.json';
-// import data from './Statistics/data.json';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" data={data} />  
+    </>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,32 @@
 import propTypes from 'prop-types';
 import s from './Statistics.module.css';
 
-export const Statistics = ({}) => {
-    return <section className={s.statistics}>
-    <h2 class="title">Upload stats</h2>
-  
-    <ul class="stat-list">
-      <li className={s.item}Name>
-        <span className={s.label}>.docx</span>
-        <span className={s.percentage}>4%</span>
-      </li>
-      <li className={s.item}>
-        <span className={s.label}>.mp3</span>
-        <span className={s.percentage}>14%</span>
-      </li>
-      <li className={s.item}>
-        <span className={s.label}>.pdf</span>
-        <span className={s.percentage}>41%</span>
-      </li>
-      <li className={s.item}>
-        <span className={s.label}>.mp4</span>
-        <span className={s.percentage}>12%</span>
-      </li>
-    </ul>
-  </section>
+export const Statistics = ({ title, data }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.statList}>
+        {data.map(({ id, label, percentage }) => (
+          <li className={s.item} key={id} style={{backgroundColor:getRandomHexColor()}}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
+
+Statistics.propTypes = {
+  title: propTypes.string,
+  data: propTypes.arrayOf(
+    propTypes.exact({
+      id: propTypes.string.isRequired,
+      label: propTypes.string.isRequired,
+      percentage: propTypes.number.isRequired,
+    }).isRequired
+  ),
+};
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
